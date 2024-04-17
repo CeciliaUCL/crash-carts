aedecec493efc86b069f6342424ec609e9b025dd
CeciliaUCL <ucab206@ucl.ac.uk>
2024-04-17 17:51:02 +0100

    Revert "final"
    This reverts commit 652f125791d946eea302955f8ef153de4956a3a7.

--- DeshSystem/conf/Config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DB_HOST = "127.0.0.1"
-DB_PORT = 3306
-DB_USER = "root"
-DB_PASS = "root"
-DB_NAME = "DeshSystem"
--- DeshSystem/tool/DBExTool.py
+++
@@ -1,9 +0,0 @@
-from tool.DBTool import DBTool
-
-
-class DBExTool(DBTool):
-
-    def sel_user_name_and_pwd(self, name, pwd):
-        sql = "select * from user where name = %s and pwd = %s"
-        result = self.sel_data_one(sql, [name, pwd])
-        return result
